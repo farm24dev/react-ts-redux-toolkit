8649cda2fc3d1c14ecb0e9a7756d23d9637ce9c3
farm24dev <farm24dec.dev@gmail.com>
2023-08-02 17:23:26 +0700

    fix fibase config

--- src/configs/firebase.ts
+++ src/configs/firebase.ts
@@ -4,17 +4,6 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAg-ZAB9D7Jrvqw9nr-yTHgn0EF8vKulxE",
-//   authDomain: "farm-react-redux-app.firebaseapp.com",
-//   projectId: "farm-react-redux-app",
-//   storageBucket: "farm-react-redux-app.appspot.com",
-//   messagingSenderId: "1082212208478",
-//   appId: "1:1082212208478:web:3ff8fa8dd7234d92cc89cf",
-//   measurementId: "G-4X8H2JJZ5E"
-// };
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_apiKey,
   authDomain: import.meta.env.VITE_FIREBASE_authDomain,
